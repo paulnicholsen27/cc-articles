5aaeb8aea18df1e6407836ad7f4c1362b899be5e
purisquiri <mau.dorigo@outlook.es>
2020-10-19 21:49:50 -0400

    Looks like done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,39 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
   
   end
+
+  def articles
+    Article.all.select do |articles|
+      articles.author == self
+    end.map do |articles|
+      articles
+    end
+  end  
+
+  def magazines
+      self.articles.map do |magazines|
+        #binding.pry
+        magazines.magazine
+      end.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+      magazines.map do |magazine|
+        #binding.pry
+        magazine.category
+      end
+  end
+
+  
 
 
 end
