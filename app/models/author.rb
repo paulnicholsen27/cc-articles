6483e68e21723187d77934f8bdae1a4d807258be
logicfirst <slee@logicfirstllc.com>
2020-10-22 23:21:32 -0400

    As good as it gets

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,44 @@
 class Author
-  attr_accessor :name
+  
+  attr_reader :name
 
+  @@all_authors = []
 
   def initialize(name)
     @name = name
-  
+    @@all_authors << self
+  end
+
+  def self.all
+    return @@all_authors
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end 
+  end
+
+  def magazines
+    author_articles = Article.all.select do |article|
+      article.author.name == self.name
+    end
+    magazines = []
+    author_articles.each.uniq do |article|
+      magazines << article.magazine 
+    end
+    return magazines
+  end
+
+  def add_article(magazine, title)
+    # Given a magazine (as Magazine instance) and a title (as a string), creates a new Article instance and associates it with that author and that magazine.
+    self.article.new(magazine, title)
+    binding.pry
+  end
+
+  def topic_areas
+    # Returns a **unique** array of strings with the categories of the magazines the author has contributed to
+    
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,10 +7,44 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author1 = Author.new("Emily Post")
+author2 = Author.new("Abigail vanBuren")
+author3 = Author.new("Minnie Mouse")
 
+mag1 = Magazine.new("Time", "News")
+mag2 = Magazine.new("People", "Pop Culture")
+mag3 = Magazine.new("Disney", "Everything Disney")
 
+art1 = Article.new(author1, mag1, "Ouch")
+art2 = Article.new(author1, mag2, "Johnny Plays Guitar")
+art3 = Article.new(author3, mag3, "Oh Mickey!")
+art4 = Article.new(author2, mag1, "My Memoir")
+# art5 = Article.new(author1, mag1, "Awww!")
 
+puts Author.all == [author1, author2, author3]
+puts author1.name == "Emily Post"
 
+puts mag1.name == "Time"
+puts mag2.name == "People"
+puts mag1.category == "News"
+puts Magazine.all == [mag1, mag2, mag3]
+
+puts art1.title == "Ouch"
+puts art3.title == "Oh Mickey!"
+puts Article.all == [art1, art2, art3, art4]
+puts art1.author.name == "Emily Post"
+puts art2.author == author1
+puts art3.magazine.name == "Disney"
+puts art4.magazine.name == "Time"
+puts author3.articles == [art3]
+puts author1.magazines == [mag1, mag2]
+
+puts "This is a test"
+puts Magazine.find_by_name("People") == mag2
+puts mag1.contributors == [author1, author2]
+puts mag3.article_titles == ["Oh Mickey"]
+puts author1.topic_areas == ["News", "Pop Culture"]
+# puts mag1.article_titles == ["Ouch", "My Memoir"]
 
 
 
