18bf26c503c71e4195a467f4c6e74f2f8e6e83d7
chirptotheeast <alyssa.easterly@yahoo.com>
2020-10-22 19:52:55 -0500

    Alyssa Easterly, done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,36 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def articles                        ## returns array of article instances author has written
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines                       ##returns unique array of Magazine instances author has contributed to 
+    self.articles.map do |article|
+      article.magazine
+    end
+  end
+
+  def add_article(magazine, title)    ## create a new article and associate it with that author and mag
+     Article.new(self, magazine, title)
+  end
+
+  def topic_areas                   ## returns array of categories st that mag author has contributed to
+    self.magazines.map do |magazine|
+      magazine.category
+    end
+  end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,53 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def contributors                  ## returns array of author instances who've written for this mag 
+    Article.all.select do |article|
+      article.magazine == self
+   end.map do |arty|
+    arty.author
+    end
+  end
+
+  def self.find_by_name(name)   ## given a str of magazine name returns first mag obj that matches
+    Magazine.all.find do |magazine|
+      magazine.name == name
+    end
+  end
+
+  def article_titles              #returns array of str of all titles articles written for mag
+    Article.all.select do |article|
+      article.magazine == self
+    end.map do |arty|
+      arty.title
+    end
+  end
+
+  def contributing_authors    #returns arr of authors who have contributed more than 2 art for mag
+    ## iterate through contributors, 
+    # if author instance occurs 2 or more times
+    # put that author in new array
+    self.contributors.select do |author|
+          self.contributors.count(author) > 2
+    end
+  end
+ 
 end
+
+     
+
+
+
+
